02a637d57d09879b26a71105a1f89ccecd93a2a3
Brittney Jackson <brnjackson@yahoo.com>
2021-09-24 20:03:47 -0400

    added gif

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Title = () => {
     return (
-        <h1>Calculating with React</h1>
+        <><h1>Calculating with React</h1>
+        <div className="icon">
+
+<iframe src="https://giphy.com/embed/5UCpmbzvZKQCfuF2P2" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+
+</div></>
     )
 }
 
